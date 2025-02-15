a0c11208eac44e3b538207e3a55e3159ded9fb6a
Faseeh <faseeh1080@gmail.com>
2025-02-15 16:36:43 +0530

    Write code to initialize sqlite database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,5 +28,8 @@ def create_app(test_config=None):
     @app.route('/')
     def hello():
         return render_template("index.html")
+    
+    from . import db
+    db.init_app(app)
 
     return app
